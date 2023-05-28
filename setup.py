3aeb53fcbbf571e0fad3c319e47074bf6d28f628
REZ3LIET <rz.samar.kale@gmail.com>
2023-05-28 09:41:30 +0530

    Created package

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
    long_description_content_type="text/markdown",
      url=" https://github.com/REZ3LIET/Pose_Estimations",
      packages=setuptools.find_packages(),
+     install_requires=['mediapipe', 'opencv-python'],
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
